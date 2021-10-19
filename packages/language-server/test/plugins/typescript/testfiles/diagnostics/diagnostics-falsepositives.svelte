b4dfd70c2aaa8d2267c27f14a42f9217e0c439c4
Lyu, Wei Da <b00605009@ntu.edu.tw>
2021-10-19 13:17:02 +0800

    add a bind test

--- packages/language-server/test/plugins/typescript/testfiles/diagnostics/diagnostics-falsepositives.svelte
+++ packages/language-server/test/plugins/typescript/testfiles/diagnostics/diagnostics-falsepositives.svelte
@@ -1,9 +1,10 @@
 <script lang="typescript">
 export let noUsedBeforeDeclare: number, anotherUsed: boolean;
 const blubb = 2;
+let p: HTMLParagraphElement;
 $: bla = blubb * 2;
 </script>
-<p on:click on:click></p>
+<p on:click on:click bind:this={p}></p>
 {bla}
 {noUsedBeforeDeclare}
 {anotherUsed}
