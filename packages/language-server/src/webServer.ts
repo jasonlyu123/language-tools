f889db48ced67ac9455ec9d2633d74c553daf6d3
Lyu, Wei Da <b00605009@ntu.edu.tw>
2022-05-30 14:20:10 +0800

    move lib loading logic out of server

--- packages/language-server/src/webServer.ts
+++ packages/language-server/src/webServer.ts
@@ -1,16 +1,13 @@
 /// <reference lib="dom" />
-
-import { createSystem, createDefaultMapFromCDN } from '@typescript/vfs';
+import { createSystem } from '@typescript/vfs';
 import ts from 'typescript';
-// import { htmlData } from 'vscode-html-languageservice/lib/esm/languageFacts/data/webCustomData';
 import {
     BrowserMessageReader,
     BrowserMessageWriter,
     createConnection
 } from 'vscode-languageserver/browser';
 import { Document, DocumentManager } from './lib/documents';
-// import { createDataProvider } from './plugins/html/dataProvider';
-import { startServerCommon } from './serverCommon';
+import { startServerCommon, SvelteLSInitializationOptions } from './serverCommon';
 import * as svelte from 'svelte/compiler';
 
 console.debug('Svelte Web server starting...');
@@ -25,27 +22,14 @@ async function main() {
 
     startServerCommon({
         connection,
-        system: ts.sys,
-        // htmlDataProvider: createDataProvider(htmlData),
         documentManger: new DocumentManager(
             (textDocument) => new Document(textDocument.uri, textDocument.text)
         ),
         svelte,
-        async initialize() {
-            const fsMap = new Map();
-
-            createDefaultMapFromCDN(
-                {
-                    target: ts.ScriptTarget.ESNext,
-                    lib: ['dom']
-                },
-                ts.version,
-                /**cache */ typeof localStorage !== 'undefined',
-                ts
-            ).then((map) => {
-                map.forEach((key, value) => fsMap.set(key, value));
-            });
-
+        initialize(initializationOptions: SvelteLSInitializationOptions | undefined) {
+            const libFiles: Record<string, string> =
+                initializationOptions?.webExtension?.libFiles ?? {};
+            const fsMap = new Map(Object.keys(libFiles).map((key) => [key, libFiles[key]]));
             const system = createSystem(fsMap);
 
             ts.sys = system;
