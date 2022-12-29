75a1e940882ab3fad52ef740b4c96c4b4c4550f2
Lyu, Wei Da <b00605009@ntu.edu.tw>
2022-12-29 14:27:01 +0800

    fix test with label detail

--- packages/language-server/test/plugins/typescript/features/CompletionProvider.test.ts
+++ packages/language-server/test/plugins/typescript/features/CompletionProvider.test.ts
@@ -1428,7 +1428,7 @@ function test(useNewTransformation: boolean) {
             );
         });
 
-        it('provides completions for object literal member',async () => {
+        it('provides completions for object literal member', async () => {
             const { completionProvider, document } = setup('object-member.svelte');
 
             const completions = await completionProvider.getCompletions(
@@ -1442,13 +1442,18 @@ function test(useNewTransformation: boolean) {
                 }
             );
 
-            const item = completions?.items.find((item) => item.label === 'hi(name)');
-            item!.insertText = harmonizeNewLines(item!.insertText)
+            const item = completions?.items.find(
+                (item) => item.label === 'hi' && item.labelDetails?.detail === '(name)'
+            );
+            item!.insertText = harmonizeNewLines(item!.insertText);
 
             delete item?.data;
 
             assert.deepStrictEqual(item, {
-                label: 'hi(name)',
+                label: 'hi',
+                labelDetails: {
+                    detail: '(name)'
+                },
                 kind: CompletionItemKind.Method,
                 sortText: '11\u0000hi\u00001',
                 preselect: undefined,
@@ -1457,9 +1462,9 @@ function test(useNewTransformation: boolean) {
                 commitCharacters: ['.', ',', ';', '('],
                 textEdit: undefined
             });
-        })
+        });
 
-        it('provides completions for class member',async () => {
+        it('provides completions for class member', async () => {
             const { completionProvider, document } = setup('object-member.svelte');
 
             const completions = await completionProvider.getCompletions(
@@ -1474,7 +1479,7 @@ function test(useNewTransformation: boolean) {
             );
 
             const item = completions?.items.find((item) => item.label === 'hi');
-            item!.insertText = harmonizeNewLines(item!.insertText)
+            item!.insertText = harmonizeNewLines(item!.insertText);
 
             delete item?.data;
 
@@ -1486,9 +1491,10 @@ function test(useNewTransformation: boolean) {
                 insertText: `hi(name: string): string {${newLine}${indent}$0${newLine}}`,
                 insertTextFormat: 2,
                 commitCharacters: undefined,
-                textEdit: undefined
+                textEdit: undefined,
+                labelDetails: undefined
             });
-        })
+        });
 
         // Hacky, but it works. Needed due to testing both new and old transformation
         after(() => {
